b46a7119316c411b1697d0add014187b411734cd
github-action-benchmark <github@users.noreply.github.com>
2024-11-26 20:45:04 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 139691b17c66aa074d2b4ae935158d4296068f72

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732643884247,
+  "lastUpdate": 1732653903321,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -41533,6 +41533,105 @@ window.BENCHMARK_DATA = {
           {
             "name": "approval-voting-parallel/approval-voting-parallel-0",
             "value": 2.4181247191699993,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "franciscoaguirreperez@gmail.com",
+            "name": "Francisco Aguirre",
+            "username": "franciscoaguirre"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "139691b17c66aa074d2b4ae935158d4296068f72",
+          "message": "Fix `XcmPaymentApi::query_weight_to_asset_fee` version conversion (#6459)\n\nThe `query_weight_to_asset_fee` function was trying to convert versions\nby using `try_as`, this function [doesn't convert from a versioned to a\nconcrete\ntype](https://github.com/paritytech/polkadot-sdk/blob/0156ca8f959d5cf3787c18113ce48acaaf1a8345/polkadot/xcm/src/lib.rs#L131).\nThis would cause all calls with a lower version to fail.\n\nThe correct function to use is the good old\n[try_into](https://github.com/paritytech/polkadot-sdk/blob/0156ca8f959d5cf3787c18113ce48acaaf1a8345/polkadot/xcm/src/lib.rs#L184).\nNow those calls work :)\n\n---------\n\nCo-authored-by: command-bot <>\nCo-authored-by: Branislav Kontur <bkontur@gmail.com>\nCo-authored-by: GitHub Action <action@github.com>",
+          "timestamp": "2024-11-26T19:24:43Z",
+          "tree_id": "e854d42229dbe6ff9912d1cb8c5dd57f13136699",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/139691b17c66aa074d2b4ae935158d4296068f72"
+        },
+        "date": 1732653883758,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52935.40000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63624.73999999999,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution/test-environment",
+            "value": 0.00001773636,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 0.00001773636,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-parallel-2",
+            "value": 2.420252163879998,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-parallel-1",
+            "value": 2.3601975511599997,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-parallel-subsystem",
+            "value": 0.47274702971997923,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.2912776288121828,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-parallel-0",
+            "value": 2.372821011570003,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel",
+            "value": 12.095668517099986,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting/test-environment",
+            "value": 0.00001780179,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 0.00001780179,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-parallel-3",
+            "value": 2.3785033110200002,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-parallel-db",
+            "value": 2.0853071180900065,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting-parallel/approval-voting-gather-signatures",
+            "value": 0.00584033166,
             "unit": "seconds"
           }
         ]
